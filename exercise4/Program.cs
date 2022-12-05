156d620d6c7ed41b1475963fdc2ef2f6e37b8303
riccoarisdy <riccoarisdy@gmail.com>
2022-12-05 16:05:33 +0700

    add node tmp to public void display

--- exercise4/Program.cs
+++ exercise4/Program.cs
@@ -47,5 +47,19 @@ namespace exercise4
             Console.WriteLine("\n The poped element is: " + top.info);
             top = top.next;
         }
+
+        public void display()
+        {
+            node tmp;
+            if (empty())
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+            }
+        }
     }
 }
