7a7ff2ba994232de38bb4b541cd1d281fc14388b
riccoarisdy <riccoarisdy@gmail.com>
2022-12-05 15:51:43 +0700

    add int info and node next to class node

--- exercise4/Program.cs
+++ exercise4/Program.cs
@@ -6,5 +6,14 @@ using System.Threading.Tasks;
 
 namespace exercise4
 {
-
+    class node
+    {
+        public int info;
+        public node next;
+        public node(int i, node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
 }
